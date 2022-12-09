6823862f705a47ee0ce217059d24618c1242c4fb
Preslav Piroev <p.piroev@abv.bg>
2022-12-09 12:57:36 +0200

    Updated the HTML and JS file to generate basic UI.

--- script.js
+++ script.js
@@ -1,81 +1,72 @@
+let playerWins = 0;
+let playerSelection = '';
+let computerWins = 0;
 
-// Picks a random number and assigns a value depending on it. 
-// If number is 1 = rock, else if number is 2 = paper, else if number is 3 = scissors.
-function getComputeChoice() {
+function getComputerChoice() {
     let randomChoice = Math.floor((Math.random() * 3) + 1);
     if (randomChoice == 1){
-        return "rock";
+        return "Rock";
     } else if (randomChoice == 2) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
-// Asks the user to submit his choice, check if it is rock, paper or scissors.
-// if yes return it, if not ask the user to input his choice again.
-function playerSelection(){
+function playRound(playerSelection, getComputerChoice){
 
-    function playerChoice() {
-        let pick = prompt("What is your choice for this round - Rock, Paper or Scissors?: ")
-        return pick.toLowerCase();
-    } 
-    
-    while (true){
-        let choice = playerChoice();
-        if (choice == "rock" || choice == "paper" || choice == "scissors"){
-            return choice
-        } else {
-            console.log(choice);;
-        }
-    } 
-}
-// Calls the two functions above and uses their return values to compare and determine who is the winner based on the conditions.
-// Returns a string with the winner and the choices.
-function playRound(playerSelection, getComputeChoice){
-
-    let player = playerSelection.toUpperCase();
-    let computer = getComputeChoice.toUpperCase();
-
+    let player = playerSelection;
+    let computer = getComputerChoice;
+    console.log(playerSelection);
+    console.log(getComputerChoice);
     if (player === computer) {
         return "It is a tie!"
     } else {
-        if (player === "ROCK" && computer === "SCISSORS") {
-            return `Player wins! ${player} beats ${computer}!`
-        } else if (player == "SCISSORS" && computer == "PAPER") {
-            return `Player wins! ${player} beats ${computer}!`
-        } else if (player == "PAPER" && computer == "ROCK"){
-            return `Player wins! ${player} beats ${computer}!`
+        if (player === "Rock" && computer === "Scissors") {
+            return "Player wins!"
+        } else if (player == "Scissors" && computer == "Paper") {
+            return "Player wins!"
+        } else if (player == "Paper" && computer == "Rock"){
+            return "Player wins!"
         } else {
-            return `Computer wins! ${computer} beats ${player}!`
+            return "Computer wins!"
         }
     }
 }
 
-// Records the winner for each round. 5 rounds are created via for loop. Return the winner and the results.
 function game(){
-    let playerWins = 0;
-    let computerWins = 0;
-    for (i = 0; i < 5; i++) {
-        let round = playRound(playerSelection(), getComputeChoice());
-        
-        if (round[0] == "C") {
-            computerWins ++;
-        } else if (round[0] == "P"){
-            playerWins ++;
-        }
+    let heading = document.querySelector('h3');
+        let round = playRound(playerSelection,getComputerChoice());
+        if (playerWins != 5 && computerWins != 5) { 
+            if (round == "Player wins!") {
+                heading.textContent = round;
+                playerWins +=1;
+            } else if (round == "Computer wins!") {
+                heading.textContent = round;
+                computerWins += 1;
+            } else {
+                heading.textContent = round;
+            }} else {
+                if (playerWins > computerWins){
+                    heading.textContent = "Player is the winner! Refresh to play again!"
+                } else {
+                    heading.textContent = "Computer is the winner! Refresh to play again!" 
+                }
+            }
+            const score = document.querySelector(".score");
+            score.textContent = `Score: Player ${playerWins} - Computer ${computerWins}`;
 
-        console.log(round);
-        console.log(`Player: ${playerWins} - Computer: ${computerWins}`);
-    } 
+        };
 
-    if (playerWins == computerWins){
-        console.log("It is a tie! The score is equal!");
-    }else if (playerWins > computerWins){
-        console.log(`Player is the winner!`);
-    } else {
-        console.log("Computer is the winner!");
-    }
-}
+const buttons = document.querySelectorAll('button');
 
-game();
+buttons.forEach(button => button.addEventListener('click', function(){
+    playerSelection = button.textContent;
+    game();
+    console.log(playerWins);
+    console.log(computerWins);
+}))
+
+
+
+   
